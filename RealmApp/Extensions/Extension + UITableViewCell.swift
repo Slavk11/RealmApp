fbd779e87ad13ffc19be3dd3c0839eca5c59a107
Stanislav Sazonov <slavk11@yandex.ru>
2023-05-28 13:42:09 +0300

    created extension for configure

---
+++ RealmApp/Extensions/Extension + UITableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  Extension + UITableViewCell.swift
+//  RealmApp
+//
+//  Created by Сазонов Станислав on 28.05.2023.
+//  Copyright © 2023 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    func configure(with taskList: TaskList) {
+        let currentTasks = taskList.tasks.filter("Is complete = false")
+        var content = defaultContentConfiguration()
+        
+        content.text = taskList.title
+        
+        if taskList.tasks.isEmpty {
+            content.secondaryText = "0"
+            accessoryType = .none
+        } else if currentTasks.isEmpty {
+            content.secondaryText = nil
+            accessoryType = .checkmark
+        } else {
+            content.secondaryText = currentTasks.count.formatted()
+            accessoryType = .none
+        }
+        
+        contentConfiguration = content
+    }
+}
